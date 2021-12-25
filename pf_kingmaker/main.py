5b498a71a6c87cc3d796508788154e15e5398e19
Matthew <mldelaney94@gmail.com>
2021-12-26 07:41:53 +1100

    Removed lang files

--- pf_kingmaker/main.py
+++ pf_kingmaker/main.py
@@ -4,7 +4,7 @@ import pinyiniser as pyer
 
 def main(path):
     zh_dict = pyer.get_dictionary(True)
-    with os.scandir(path) as d:
+    with os.scandir('./' + path) as d:
         for file in d:
             contents = read_file(file.path)
             add_pinyin(dict(contents), zh_dict)
@@ -15,15 +15,12 @@ def read_file(path):
         return json.load(f)
 
 def write_file(path, content):
-    with open(path + '_edited', 'w') as f:
+    with open(path, 'w') as f:
         f.write(json.dumps(content, ensure_ascii=False, indent=2))
-        exit()
 
 def add_pinyin(dict_object, zh_dict):
     for item in dict_object['strings']:
         item['Value'] = pyer.add_pinyin(item['Value'], zh_dict)
-    
-
 
 if __name__ == "__main__":
-    main('./lang_files')
+    main('lang_files')
